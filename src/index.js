125a3db651ceae2cca739e5dce2f1ee90f2a4422
aigerim99k <aigerim.99k@gmaol.com>
2021-03-15 05:25:41 +0500

    init: solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
+if(!Array.isArray(array)|| !array.length){
   return 0;
+}
+else{
+  let min=array[0];
+  for(let i=0;i<array.length;i++){
+    if(min>array[i]){
+      min=array[i]
+    }
+  }
+  return min;
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array)|| !array.length){
+    return 0;
+  }
+  else{
+    let max=array[0];
+    for(let i=0;i<array.length;i++){
+      if(max<array[i]){
+        max=array[i]
+      }
+    }
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array)|| !array.length){
+    return 0;
+  }
+  else{
+    let sum=0;
+    for(let i=0;i<array.length;i++){
+      sum+=array[i];
+    }
+    return sum/array.length;  
+  }
 }
